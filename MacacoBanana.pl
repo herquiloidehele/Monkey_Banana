e5f09b3dc4f08468b5b8d998c3d7efb558da2000
Herquiloide Raimundo Hele <herquiloide@gmail.com>
2018-04-10 12:46:47 +0200

    Renomeando as variaveis

--- MacacoBanana.pl
+++ MacacoBanana.pl
@@ -1,33 +1,36 @@
-move(estado(centro,na_caixa,centro,centro,possui_vara,nao_possui),
+
+%% Acccao pra o macaco pegar a banana
+movimento(estado(no_centro,em_cima_cadeira,no_centro,no_centro,possui_vara,nao_possui_banana),
       pegar_banana,
-      estado(centro,na_caixa,centro,centro,possui_vara,possui)).
+      estado(no_centro,em_cima_cadeira,no_centro,no_centro,possui_vara,possui_banana)).
 
-move(estado(P,no_chao,P,P,possui_vara,nao_possui),
+
+movimento(estado(P,no_chao,P,P,possui_vara,nao_possui_banana),
       subir_caixa,
-      estado(P,na_caixa,P,P,possui_vara,nao_possui)).
+      estado(P,em_cima_cadeira,P,P,possui_vara,nao_possui_banana)).
 
-move(estado(P1,no_chao,P1,P1,possui_vara,nao_possui),
+movimento(estado(P1,no_chao,P1,P1,possui_vara,nao_possui_banana),
       empurrar_caixa(P1,P2),
-      estado(P2,no_chao,P2,P2,possui_vara,nao_possui)).
+      estado(P2,no_chao,P2,P2,possui_vara,nao_possui_banana)).
 
 
-move(estado(P1,no_chao,B,P1,nao_possui_vara,nao_possui),
+movimento(estado(P1,no_chao,B,P1,nao_possui_vara,nao_possui_banana),
       pegar_vara,
-      estado(P1,no_chao,B,P1,possui_vara,nao_possui)).
+      estado(P1,no_chao,B,P1,possui_vara,nao_possui_banana)).
 
 
-move(estado(P1,no_chao,B,P1,possui_vara,nao_possui),
+movimento(estado(P1,no_chao,B,P1,possui_vara,nao_possui_banana),
       caminhar_com_vara(P1,P2),
-      estado(P2,no_chao,B,P2,possui_vara,nao_possui)).
+      estado(P2,no_chao,B,P2,possui_vara,nao_possui_banana)).
 
 
-move(estado(P1,no_chao,B,P3,nao_possui_vara,nao_possui),
+movimento(estado(P1,no_chao,B,P3,nao_possui_vara,nao_possui_banana),
       caminhar(P1,P2),
-      estado(P2,no_chao,B,P3,nao_possui_vara,nao_possui)).
+      estado(P2,no_chao,B,P3,nao_possui_vara,nao_possui_banana)).
 
-consegue(estado(_,_,_,_,_,possui)):-!.
+consegue(estado(_,_,_,_,_,possui_banana)):-!.
 consegue(Estado1):-
-    move(Estado1,_,Estado2),
+    movimento(Estado1,_,Estado2),
     consegue(Estado2).
 
 
